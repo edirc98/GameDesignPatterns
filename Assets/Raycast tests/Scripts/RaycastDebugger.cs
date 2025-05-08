2ab24ee282db58aeaed84a522f2ac302ed1b35f3
Edgar <edirc98@gmail.com>
2025-05-08 11:21:00 +0200

    Raycast Debugger Visual Ray color configuration
    Ray, Penetration Ray, hit point and hit normal can be changed of color from inspector

--- Assets/Raycast tests/Scripts/RaycastDebugger.cs
+++ Assets/Raycast tests/Scripts/RaycastDebugger.cs
@@ -37,26 +37,47 @@ public class RaycastDebugVisual
     public float LifeTime { set { lifeTime = value; } }
     public bool RayExpired => lifeTime <= 0.0f && lifeTime != -1.0f;
 
+    //Ray Colors
+    private Color baseRayColor = Color.yellow;
+    private Color penetrationRayColor = Color.magenta;
+    private Color hitPointColor = Color.red;
+    private Color hitNormalColor = Color.blue;
+
+
+    #region CUSTOM METHODS
     public void RayTick()
     {
-        if(lifeTime != -1.0f) lifeTime -= Time.deltaTime;
+        if (lifeTime != -1.0f) lifeTime -= Time.deltaTime;
     }
+
+    public void SetRayVisualColors(Color BaseColor, Color PenetrationColor, Color HitPointColor, Color HitNormalColor)
+    {
+        baseRayColor = BaseColor;
+        penetrationRayColor = PenetrationColor;
+        hitPointColor = HitPointColor;
+        hitNormalColor = HitNormalColor;
+    }
+
 
     public void DarwRaycast()
     {
         //MAIN RAY
-        Gizmos.color = Color.yellow;
+        Gizmos.color = baseRayColor;
         Gizmos.DrawRay(rayOrigin, hitDir * hitDistance);
+
         //PENETRATION RAY
-        Gizmos.color = Color.magenta;
+        Gizmos.color = penetrationRayColor;
         Gizmos.DrawRay(hitPoint, hitDir * hitPenetrationDistance);
 
-        Gizmos.color = Color.red;
+        //HIT POINT SPEHERE
+        Gizmos.color = hitPointColor;
         Gizmos.DrawSphere(hitPoint, 0.1f);
 
-        Gizmos.color = Color.blue;
+        //HIT NORMAL RAY
+        Gizmos.color = hitNormalColor;
         Gizmos.DrawRay(hitPoint, hitNormal);
     }
+    #endregion
 }
 public class RaycastDebugger : Singleton<RaycastDebugger>
 {
@@ -67,7 +88,10 @@ public class RaycastDebugger : Singleton<RaycastDebugger>
     public float RayLifeTime;
 
     [Header("Ray Colors")]
-    public Color BaseRayColor = new Color();
+    public Color BaseRayColor = Color.yellow;
+    public Color PenetrationRayColor = Color.magenta;
+    public Color HitPointColor = Color.red;
+    public Color HitNormalColor = Color.blue;
 
     [Header("Stored Raycasts")]
     private List<RaycastDebugVisual> raycastsDebugVisuals = new List<RaycastDebugVisual>();
@@ -94,6 +118,7 @@ public class RaycastDebugger : Singleton<RaycastDebugger>
     public void AddRaycastVisualDebug(Vector3 RayOrigin, Vector3 RayDir, float RayDistance, Vector3 HitNormal, Vector3 HitPoint)
     {
         RaycastDebugVisual raycastVisual = new RaycastDebugVisual(RayOrigin, RayDir, RayDistance, HitNormal, HitPoint);
+        raycastVisual.SetRayVisualColors(BaseRayColor, PenetrationRayColor, HitPointColor, HitNormalColor);
         if (AutoDeleteAfterTime)
         {
             raycastVisual.LifeTime = RayLifeTime;
@@ -104,6 +129,7 @@ public class RaycastDebugger : Singleton<RaycastDebugger>
     public void AddRaycastVisualDebug(Vector3 RayOrigin, Vector3 RayDir, float RayDistance, float PenetrationDistance, Vector3 HitNormal, Vector3 HitPoint)
     {
         RaycastDebugVisual raycastVisual = new RaycastDebugVisual(RayOrigin, RayDir, RayDistance, PenetrationDistance, HitNormal, HitPoint);
+        raycastVisual.SetRayVisualColors(BaseRayColor, PenetrationRayColor, HitPointColor, HitNormalColor);
         if (AutoDeleteAfterTime)
         {
             raycastVisual.LifeTime = RayLifeTime;
